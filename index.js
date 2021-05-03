c11ddbd4a7cf4045a35b40f9d41e98654c9a4b4c
Magnus Brevik <mbremyk@hotmail.no>
2021-05-03 22:47:42 +0200

    Add functionality for fetching data from server and displaying it

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const baseUrl = 'https://snau.herokuapp.com/'
+
 let loadPage = (event, word) => {
     let tablinks = document.getElementsByClassName('tablink');
     for (i = 0; i < tablinks.length; i++) {
@@ -12,6 +14,36 @@ let loadPage = (event, word) => {
 
     let content;
     if (word == 'heim') content = document.getElementById('heim');
-    else content = document.getElementById('liste');
+    else {
+        content = document.getElementById('liste');
+        getData(word)
+            .then(data => {
+                let table = content.getElementsByClassName('list')[0];
+                table.innerHTML = "";
+                let headerRow = document.createElement('TR');
+                let keys = Object.keys(data[0]);
+                keys.map(key => {
+                    let header = document.createElement('TH');
+                    header.innerHTML = key;
+                    headerRow.appendChild(header);
+                });
+                table.appendChild(headerRow);
+                data.map(obj => {
+                    let row = document.createElement('TR');
+                    keys.map(key => {
+                        let cell = document.createElement('TD');
+                        cell.innerHTML = obj[key];
+                        row.appendChild(cell);
+                    });
+                    table.appendChild(row);
+                });
+            });
+    }
     content.className += ' active';
+}
+
+let getData = (extension) => {
+    let url = `${baseUrl}${extension}`;
+    return fetch(url)
+        .then(data => data.json());
 }

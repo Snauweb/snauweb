b4b0734b3bba4fc4b55331b2c77208325985dd91
Nora <nora.heiberg@gmail.com>
2021-11-10 20:35:20 +0100

    oppdatert klikkeren slik at flere alpakker kommer opp når man klikker på en vilkålig bugge

--- alpakka_clicker.js
+++ alpakka_clicker.js
@@ -1,4 +1,22 @@
+let bilde= undefined;
+let alpakkaBoks = undefined;
+
 function init(){
-    let bilde = document.querySelector('img');
+    bilde = document.querySelector('img');
+    bilde.addEventListener('click',alpakkaKlikk)
+    alpakkaBoks = document.querySelector('.alpakkaBoks')
+    console.log(bilde, alpakkaBoks)
 }
 window.addEventListener('load', init)
+
+function alpakkaKlikk(){
+    console.log('klikk')
+    if(bilde == undefined || alpakkaBoks == undefined){
+        return;
+    }
+
+    let newAlpakka = bilde.cloneNode(true);
+    newAlpakka.addEventListener('click',alpakkaKlikk)
+    alpakkaBoks.appendChild(newAlpakka);
+
+}

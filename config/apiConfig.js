ef8fcdd28542c09748a3551db8e3e7bd3d2e06f9
Grimmbol <krogstie.oystein@gmail.com>
2021-12-08 18:42:56 +0100

    add html mock of new filterable and searchable forslag-list and some related new assets

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,7 +1,7 @@
 export { config };
 
 let config = {
-    "debug": "false",
+    "debug": "true",
     "debugURI": "http://localhost:8000/snauwebAPI/api.py",
     "apiURI": "https://snaustrinda.samfundet.no/api/api.py"
 }

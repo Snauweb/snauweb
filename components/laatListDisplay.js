b18b2fa264b24b46ff1ec886b9b3eccb68f19e88
Grimmbol <krogstie.oystein@gmail.com>
2022-02-23 18:27:50 +0100

    add data based laat list html generation

--- components/laatListDisplay.js
+++ components/laatListDisplay.js
@@ -1,12 +1,46 @@
 export { LaatListDisplay }
 import { DataList } from "./dataList.js"
 
-class LaatListDisplay extends dataList {
+class LaatListDisplay extends DataList {
 
   constructor() {
     super();
+    this.render();
   }
 
+  // Overwrite DataList implementation
+  render() {
+    // Copy old top level element (non-recursive, no children are included)
+    let newListWrapper = this.listWrapperElem.cloneNode();
+    
+    // iterate over all displayData objects
+    for(let dataObj of this.displayData) {
+      
+      // Copy the list item template
+      let curListItem = this.listElemWrap.cloneNode(true);
+
+      // First we set up the title link
+      let titleElem = curListItem.querySelector(".navn");
+      let laatLinkElem = document.createElement("a");
+
+      laatLinkElem.setAttribute("href", dataObj["URL"]);
+      laatLinkElem.textContent = dataObj["navn"];
+      
+      titleElem.appendChild(laatLinkElem);
+
+      // Then the genere in parentheses
+      let genereElem = curListItem.querySelector(".sjanger");
+      genereElem.textContent = "(" + dataObj["sjanger"] + ")";
+
+      // Add to full list
+      newListWrapper.appendChild(curListItem);
+    }
+    
+    // Replace old list with new
+    this.replaceChild(newListWrapper, this.listWrapperElem);
+    this.listWrapperElem = newListWrapper;
+  }
+  
   // **** BUILT-INS ****
 
   // Returns a list of names of attributes
